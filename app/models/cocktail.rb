f5b1eddc877b10a11397dfb8f3534f275fdd941e
noel balthazart <n031.b@hotmail.com>
2020-11-12 17:11:51 +0000

    routes adapted

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,6 +2,6 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
-  validates :name, :photo_url, presence: true
+  validates :name, presence: true
   validates :name, uniqueness: true
 end
