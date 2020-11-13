a628842b8eb8bdad3827d938978e096f0a7d298c
noel balthazart <n031.b@hotmail.com>
2020-11-13 15:24:57 +0000

    cloudinary logic added

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,7 +1,8 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_one_attached :photo
 
-  validates :name, :photo_url, :description, presence: true
+  validates :name, :description, presence: true
   validates :name, uniqueness: true
 end
